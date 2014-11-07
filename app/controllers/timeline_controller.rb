71b6e27abb5de44376248b5ac5e5ba493b5f00b0
Jessica Lins <jessicaalins@gmail.com>
2014-11-07 18:22:42 -0300

    Adding seeds and removing timeline_controller

--- app/controllers/timeline_controller.rb
+++
@@ -1,5 +0,0 @@
-class TimelineController < ApplicationController
-  def index
-  	@tweets = current_user.tweets.order('created_at DESC').includes(:user)
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+data = {}
+data[:ted] = ['Ted Mosby']
+data[:marshall] = ['Marshall Eriksen']
+data[:robin] = ['Robin Scherbatsky']
+data[:barney] = ['Barney Stinson']
+data[:lily] = ['Lily Aldrin']
+data[:tracy] = ['Tracy McConnell']
+data[:victoria] = ['Victoria']
+
+data.each do |k, v|
+  u = User.where(email: "#{k}@gmail.com", name: v[0]).first_or_initialize
+  u.password = "#{k}twitter"
+  u.avatar = File.open "bootstrap/#{k}.jpg"
+  u.username = "#{k}"
+  u.save!
+end
+
+User.all.each do |user|
+	50.times do
+		data = {content: "Tweet sample ##{Random.rand(1000)}",
+						user_id: user.id,
+						author: user.name
+					 }
+		Tweet.create! data
+	end
+
+	samples = users.sample(2)
+	user.user_followers.create(user_id: samples[0].id, follower_id: samples[1].id) if samples[0].id != samples[1].id
+end
