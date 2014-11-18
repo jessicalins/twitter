a35a7300e510386db5a7a19f9d13ee83b7b35a04
Jessica Lins <jessicaalins@gmail.com>
2014-11-18 15:22:12 -0300

    Adding pagination with kaminari

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'bootstrap-sass', '~> 3.2.0'
 gem 'autoprefixer-rails'
 gem 'carrierwave'
 gem "mini_magick", "~> 4.0.0.rc"
+gem 'kaminari'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
 	def index
-		@tweets = Tweet.where(user_id: [[current_user.id] | current_user.following_ids]).order('created_at DESC')
+		@tweets = Tweet.where(user_id: [[current_user.id] | current_user.following_ids]).order('created_at DESC').page(params[:page]).per(10)
   	@user = current_user
 		@tweet = Tweet.new		
   end
