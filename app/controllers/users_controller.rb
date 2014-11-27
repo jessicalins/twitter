e9b62b08d10ee92742eaec82bae19821168b451a
Jessica Lins <jessicaalins@gmail.com>
2014-11-27 18:33:03 -0300

    Adding error page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
 	respond_to :html, :js
 	def show
-		@user = User.find_by_username(params[:id])
-		@tweets = @user.tweets.order('created_at DESC').includes(:user).page(params[:page])
-		respond_with @tweets, location: user_path(@user)
+		@user = User.find_by_username(params[:id]) 
+		if @user
+			@tweets = @user.tweets.order('created_at DESC').includes(:user).page(params[:page])
+			respond_with @tweets, location: user_path(@user)
+		else
+			not_found
+		end
+	end
+
+	def not_found
+		render 'erro404'
 	end
 end
