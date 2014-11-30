311fbbf8ed2418f26061041557980d4d078a77e7
Lucas Eduardo <lucasecf@gmail.com>
2014-11-30 20:45:32 -0300

    Refactoring tweets count to support multiple pages

--- app/controllers/follow_controller.rb
+++ app/controllers/follow_controller.rb
@@ -10,7 +10,7 @@ class FollowController < ApplicationController
 	end
 
 	def follow
-		@user = User.find(params[:id])
+		@user = User.find_by_username(params[:user_id])
 		@user.user_followers.create(follower_id: current_user.id)
 		respond_to do |format|
 			format.js { render 'after_follow' }
@@ -18,7 +18,7 @@ class FollowController < ApplicationController
 	end
 
 	def unfollow
-		@user = User.find(params[:user_id])
+		@user = User.find_by_username(params[:user_id])
 		UserFollower.where(follower_id: current_user.id, user_id: @user.id).destroy_all
 		respond_to do |format|
 			format.js { render 'after_follow' }
