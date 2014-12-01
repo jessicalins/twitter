af3398f2b7541d410661c4a31fde195b1390f897
Jessica Lins <jessicaalins@gmail.com>
2014-12-01 15:16:02 -0300

    Fixed following/followers page

--- app/controllers/follow_controller.rb
+++ app/controllers/follow_controller.rb
@@ -1,11 +1,11 @@
 class FollowController < ApplicationController
 	def following
-		@user = User.find(params[:user_id])
+		@user = User.find_by_username(params[:user_id])
 		@following = @user.following
 	end
 
 	def followers
-		@user = User.find(params[:user_id])
+		@user = User.find_by_username(params[:user_id])
 		@followers = @user.followers
 	end
 
